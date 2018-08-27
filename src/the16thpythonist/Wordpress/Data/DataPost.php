ea854b3b16b237efec12db5318124feddafbe5b3
the16thpythonist <jonseb1998@gmail.com>
2018-08-27 15:56:26 +0200

    Work in progress version 0

--- src/the16thpythonist/Wordpress/Data/DataPost.php
+++ src/the16thpythonist/Wordpress/Data/DataPost.php
@@ -8,8 +8,40 @@
 
 namespace the16thplayer\Wordpress\Data;
 
+// All the uses need to be hardcoded. If a new type has been created it needs to be updated here
+use the16thplayer\Wordpress\Data\Type\JSONFilePost;
+
 
 class DataPost
 {
+    public static $POST_TYPE;
+    public static $REGISTRATION;
 
+    /*
+     * Here a associative array needs to be hardcoded, which assignes each "file extension" (really just a string with
+     * the type name) the according class to be used in such a case. For example when a new one is to be created.
+     */
+    public static $TYPES = array(
+        'JSON'          => JSONFilePost::class
+    );
+
+    public static function create(string $filename) {
+        $split = explode($filename, '.');
+        $name = $split[0];
+        $type = strtoupper($split[1]);
+
+        // TODO: Actually create a new post in wordpress (Also needs checking if already exists)
+
+        // Creating a wrapper object
+        $class = static::$TYPES[$type];
+    }
+
+    public static function register(string $post_type) {
+        static::$POST_TYPE = $post_type;
+
+        $registration = new DataPostRegistration($post_type);
+        $registration->register();
+
+        static::$REGISTRATION = $registration;
+    }
 }
