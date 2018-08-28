22da81bc66baa45591b314ec2ccce7ad0fbc13c4
the16thpythonist <jonseb1998@gmail.com>
2018-08-28 14:16:06 +0200

    work in progress version 0

--- src/the16thpythonist/Wordpress/Data/DataPost.php
+++ src/the16thpythonist/Wordpress/Data/DataPost.php
@@ -11,20 +11,63 @@ namespace the16thplayer\Wordpress\Data;
 // All the uses need to be hardcoded. If a new type has been created it needs to be updated here
 use the16thplayer\Wordpress\Data\Type\JSONFilePost;
 
-
+/**
+ * Class DataPost
+ *
+ *
+ *
+ * @package the16thplayer\Wordpress\Data
+ */
 class DataPost
 {
+    /**
+     * After the "register" method was called to register the new post type with wordpress, the actual slug of that
+     * new custom post type is stored in this field.
+     *
+     * @var string
+     */
     public static $POST_TYPE;
+
+    /**
+     * After the register method was called on this class, this field stores the DataPostRegistration object, that was
+     * used for the registration process. It contains information such as the exact slug of the type taxonomy.
+     *
+     * @var DataPostRegistration
+     */
     public static $REGISTRATION;
 
     /*
-     * Here a associative array needs to be hardcoded, which assignes each "file extension" (really just a string with
+     * Here an associative array needs to be hardcoded, which assignes each "file extension" (really just a string with
      * the type name) the according class to be used in such a case. For example when a new one is to be created.
+     */
+    /**
+     * This array contains the string names of different types (the ones being used as file extensions) as keys and the
+     * according CLASSES for the wrapper class representing that type (having the correct encoder and decoder for the
+     * data structure).
+     * The type is case insensitive, thus the type names are all in lower case.
+     *
+     * @var array
      */
     public static $TYPES = array(
         'json'          => JSONFilePost::class
     );
 
+    /**
+     * Based on the given filename, loads that data post and returns the according post wrapper object.
+     *
+     * The objects returned by this method implement the "FileData" interface, which means the data structure, that
+     * is saved within the post can be extracted from it by calling the "load" method and a new one can be saved using
+     * the "save" method. Beware the types though!
+     *
+     * CHANGELOG
+     *
+     * Added 28.08.2018
+     *
+     * @since 0.0.0.0
+     *
+     * @param string $filename
+     * @return mixed
+     */
     public static function load(string $filename) {
         $file = static::explodeFilename($filename);
         $query = static::getWpQuery($filename);
@@ -35,6 +78,25 @@ class DataPost
         return static::createWrapperObject((string) $post_id, $file['type']);
     }
 
+    /**
+     * Creates a new "file" data post by the given file name & returns the wrapper object to the post
+     *
+     * Based on the type given in the type extension, different classes are used to create an instance. But all those
+     * classes implement the "FileData" interface, which means, the data structure, that is saved inside of them can
+     * be retrieved by using the "load" method and new content can be saved by using the "save" method.
+     *
+     * When using this class it is important to be sure, that based on the file extension the correct data type is being
+     * assumed when loading and saving.
+     *
+     * CHANGELOG
+     *
+     * Added 28.08.2018
+     *
+     * @since 0.0.0.0
+     *
+     * @param string $filename
+     * @return mixed
+     */
     public static function create(string $filename) {
         $file = static::explodeFilename($filename);
 
@@ -45,6 +107,19 @@ class DataPost
         return static::createWrapperObject((string) $post_id, $file['type']);
     }
 
+    /**
+     * Instantiates and returns a wrapper object of the class corresponding to the given type, using the given post id
+     *
+     * CHANGELOG
+     *
+     * Added 28.08.2018
+     *
+     * @since 0.0.0.0
+     *
+     * @param string $post_id
+     * @param string $type
+     * @return mixed
+     */
     private static function createWrapperObject(string $post_id, string $type) {
         // Creating a wrapper object
         $class = static::$TYPES[$type];
@@ -52,6 +127,19 @@ class DataPost
         return $object;
     }
 
+    /**
+     * Calls the wordpress function, that actually inserts a post with the given title & type tax. term, returns post ID
+     *
+     * CHANGELOG
+     *
+     * Added 28.08.2018
+     *
+     * @since 0.0.0.0
+     *
+     * @param string $title
+     * @param string $type
+     * @return mixed
+     */
     private static function createWordpressPost(string $title, string $type) {
         $args = array(
             'post_type'         => static::$POST_TYPE,
@@ -64,14 +152,37 @@ class DataPost
         return wp_insert_post($args);
     }
 
+    /**
+     * Boolean return of whether or not a file with the given filename already exists.
+     *
+     * CHANGELOG
+     *
+     * Added 28.08.2018
+     *
+     * @since 0.0.0.0
+     *
+     * @param string $filename
+     * @return bool
+     */
     public static function exists(string $filename) {
         $query = static::getWpQuery($filename);
         return $query->post_count !== 0;
     }
 
+    /**
+     * Returns the WP_Query object, which creates an SQL query based on the name and type in the given filename.
+     *
+     * CHANGELOG
+     *
+     * Added 28.08.2018
+     *
+     * @since 0.0.0.0
+     *
+     * @param string $filename
+     * @return mixed
+     */
     private static function getWpQuery(string $filename) {
         $file = static::explodeFilename($filename);
-
         /*
          * Originally there is a problem with using the WP_Query for such a request. Filtering by the taxonomy term is
          * easy using the "tax_query" parameter. But the WP_Query doesnt support filtering by the actual title.
@@ -98,6 +209,26 @@ class DataPost
         return $query;
     }
 
+    /**
+     * Returns an assoc array with two elements, them being the file name and type (extension) if given a "filename"
+     *
+     * The DataPost system works on the basis of mapping "filename" to wordpress posts. These file names are supposed
+     * to be like "normal" file names, in having a name and a type extension seperated by a period. For example
+     * 'prices.json' would be such a name.
+     * Often in the code the type and name string are needed separately. This method returns an array containing the
+     * actual name and the type string. For example: array('name' => 'prices', 'type' => 'json').
+     * The type will be represented in lower letters only, because the type string is case insensitive for all
+     * references throughout the code.
+     *
+     * CHANGELOG
+     *
+     * Added 28.08.2018
+     *
+     * @since 0.0.0.0
+     *
+     * @param string $filename
+     * @return array
+     */
     private static function explodeFilename(string $filename) {
         $explode = explode('.', $filename);
         $name = $explode[0];
@@ -105,6 +236,17 @@ class DataPost
         return array("name" => $name, "type" => $type);
     }
 
+    /**
+     * Registers the new CPT in wordpress
+     *
+     * CHANGELOG
+     *
+     * Added 28.08.2018
+     *
+     * @since 0.0.0.0
+     *
+     * @param string $post_type
+     */
     public static function register(string $post_type) {
         add_filter('posts_where', array(static::class, 'queryTitleFilter'), 10, 2);
 
